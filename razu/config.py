0ccf945533c477f2a5787e4f7825a993100b59e8
Pre-ingest station <depots@razu.nl>
2024-09-02 12:14:44 +0200

    getters

--- razu/config.py
+++ razu/config.py
@@ -13,11 +13,14 @@ class Config:
             self.initialized = True
             self.RAZU_base_URI = "https://data.razu.nl/"
             self.RAZU_file_id = "NL-WbDRAZU"
-            self.creator = None     # voorbeeld: "G321" voor Gemeente Houten
-            self.archive = None     # / toegang, voorbeeld "661" , als in Mais Flexis
+            self._creator = None     # voorbeeld: "G321" voor Gemeente Houten
+            self._archive = None     # / toegang, voorbeeld "661" , als in Mais Flexis
             self.save = False
             self.save_dir = "test"
-        self.filename_prefix = f"{self.RAZU_file_id}-{self.creator}-{self.archive}" # voorbeeld "NL-WbDRAZU-G312-661"
+        self._update_prefixes()
+
+    def _update_prefixes(self):
+        self.filename_prefix = f"{self.RAZU_file_id}-{self._creator}-{self._archive}" # voorbeeld "NL-WbDRAZU-G312-661"
         self.URI_prefix = f"{self.RAZU_base_URI}{self.filename_prefix}"             # voorbeeld ""https://data.razu.nl/NL-WbDRAZU-G312-661"
 
     @classmethod
@@ -25,10 +28,17 @@ class Config:
         instance = cls()
         instance.save = save
         instance.save_dir = save_dir
-        instance.creator = creator
-        instance.archive = archive
-        instance.filename_prefix = f"{instance.RAZU_file_id}-{instance.creator}-{instance.archive}"
-        instance.URI_prefix = f"{instance.RAZU_base_URI}{instance.filename_prefix}"
+        instance._creator = creator
+        instance._archive = archive
+        instance._update_prefixes()
         
         if instance.save and instance.save_dir is not None:
             os.makedirs(instance.save_dir, exist_ok=True)
+
+    @property
+    def creator(self):
+        return self._creator
+
+    @property
+    def archive(self):
+        return self._archive
