062ee9b21eebdfe82b0394b33d8bf012919509bc
RVoorburg-RAZU <r.voorburg@razu.nl>
2024-10-15 16:59:28 +0200

    wip

--- razu/sip.py
+++ razu/sip.py
@@ -9,15 +9,19 @@ from .concept_resolver import ConceptResolver
 from .meta_object import MetaObject
 from .meta_graph import MetaGraph
 from .manifest import Manifest
+from .id_manager import IDManager
+import razu.util as util
 
 
 class Sip:
-    def __init__(self, sip_dir, archive_creator_id, dataset_id: str, newest_id=None) -> None:
+    """
+    A class representing a SIP (Submission Information Package)
+    """
+
+    def __init__(self, sip_dir, archive_creator_id, dataset_id: str) -> None:
         self.sip_dir = sip_dir
         self.archive_creator_id = archive_creator_id
         self.dataset_id = dataset_id
-
-        self.newest_id = 0
         
         actoren = ConceptResolver('actor')
         self.archive_creator_uri = actoren.get_concept_uri(self.archive_creator_id)
@@ -29,15 +33,14 @@ class Sip:
 
         self.graph = MetaGraph()
         self.manifest = Manifest(self.sip_dir, self.cfg.manifest_filename)
+
+        self.id_manager = IDManager()
+        for filename in self.manifest.get_filenames():
+            if filename.endswith(f'{self.cfg.metadata_suffix}.json'):
+                self.id_manager.register_id(util.extract_id_from_filename(filename))
         
-        if newest_id is None:
-            self.newest_id = self.manifest.newest_id
-            if self.manifest.newest_id > 0:
-                self._load_graph()
-        else:
-            if self.manifest.newest_id > 0:
-                print ("Existing files will be overwritten.")
-            self.newest_id = newest_id
+        if self.id_manager.get_count() > 0:
+            self._load_graph()
 
     def _load_graph(self):
         for filename in self.manifest.get_filenames():
@@ -49,39 +52,50 @@ class Sip:
 
     def save_graph(self):
         """
-        For each entity in the graph with a URI, create a MetaObject, fill it with the relevant
-        properties, and save it as a JSON-LD file.
+        For each unique entity in the graph with a URI, create a MetaObject, fill it with the relevant
+        properties (and blank nodes), and store it.
         """
-        def add_related_triples_to_meta_object(meta_object, node):
+        def add_related_triples_to_meta_object(meta_object, node, processed_nodes=None):
             """ Recursively add triples related to the given node (including blank nodes) to the MetaObject. """
+            if processed_nodes is None:
+                processed_nodes = set()  # Houd de verwerkte nodes bij
+
+            if node in processed_nodes:
+                return  # Voorkom dubbele verwerking van dezelfde node
+
+            processed_nodes.add(node)
+
             for predicate, obj in self.graph.predicate_objects(node):
                 meta_object.add(predicate, obj)
+
                 if isinstance(obj, BNode):
-                    add_related_triples_to_meta_object(meta_object, obj)
+                    # Verwerk blank nodes recursief
+                    add_related_triples_to_meta_object(meta_object, obj, processed_nodes)
+
+        processed_subjects = set()  # Houd bij welke subjects zijn verwerkt
 
         for subject in self.graph.subjects():
-            if isinstance(subject, URIRef): 
+            if isinstance(subject, URIRef) and subject not in processed_subjects:
                 meta_object = MetaObject(uri=subject)
                 add_related_triples_to_meta_object(meta_object, subject)
-                self.store_object(meta_object)
+                self.save_object_metadata(meta_object)
+                processed_subjects.add(subject)  # Voeg het subject toe aan de set van verwerkte subjects
 
-    def create_object(self, **kwargs):
-        # TODO: identifiers kunnen de mist in gaan als zowel een uri wordt meegegeven als soms ook vertrouwd wordt op
-        # automatische toekenning met self.newest_id
-        valid_kwargs = {}
-        if 'entity_id' not in kwargs:
-            self.newest_id += 1
-            valid_kwargs['entity_id'] = self.newest_id
+
+    def create_object(self, entity_id = None, rdf_type = None):
+        kwargs = {}
+        if entity_id is None:
+            kwargs['entity_id'] = self.id_manager.generate_id()
         else:
-            valid_kwargs['entity_id'] = kwargs['entity_id']
+            kwargs['entity_id'] = self.id_manager.register_id(entity_id)
 
-        if 'rdf_type' in kwargs:
-            valid_kwargs['rdf_type'] = kwargs['rdf_type']
-        return MetaObject(**valid_kwargs)
+        if rdf_type is not None:
+            kwargs['rdf_type'] = rdf_type
 
-    def store_object(self, object: MetaObject, source_dir = None):
-        # process the metadata-file:
-        self.graph += object
+        return MetaObject(**kwargs)
+
+
+    def save_object_metadata(self, object: MetaObject, source_dir = None):
         object.save()
         md5checksum = self.manifest.calculate_md5(object.meta_file_path)
         md5date = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
@@ -91,22 +105,34 @@ class Sip:
             "Source": self.archive_creator_uri,
             "Dataset": self.dataset_id
         })
+        self.manifest.save()
+
+
+    def save_object_file(self, object: MetaObject, source_dir = None):
+        origin_filepath = os.path.join(source_dir, object.original_filename)
+        dest_filepath  = os.path.join(self.sip_dir, object.filename)
+        shutil.copy2(origin_filepath, dest_filepath)
+
+        self.manifest.add_entry(object.filename, object.md5checksum, object.checksum_datetime) 
+        self.manifest.update_entry(object.filename, {
+            "ObjectUID": object.object_identifier,
+            "Source": self.archive_creator_uri,
+            "Dataset": self.dataset_id,
+            "FileFormat": object.fileformat_uri,
+            "OriginalFilename": object.original_filename
+        })
+        self.manifest.save()
+
+
+    def store_object(self, object: MetaObject, source_dir = None):
+        # process the metadata-file:
+        self.graph += object
+        self.save_object_metadata(object, source_dir)
         
         # process the (optional) referenced file:
         if source_dir is not None:
-            origin_filepath = os.path.join(source_dir, object.original_filename)
-            dest_filepath  = os.path.join(self.sip_dir, object.filename)
-            shutil.copy2(origin_filepath, dest_filepath)
-
-            self.manifest.add_entry(object.filename, object.md5checksum, object.checksum_datetime) 
-            self.manifest.update_entry(object.filename, {
-                "ObjectUID": object.object_identifier,
-                "Source": self.archive_creator_uri,
-                "Dataset": self.dataset_id,
-                "FileFormat": object.fileformat_uri,
-                "OriginalFilename": object.original_filename
-            })
-        self.manifest.save()
+            self.save_object_file(object, source_dir)
+            
 
     def validate(self):
         self.manifest.verify()
